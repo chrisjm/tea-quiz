68a58eb05d6ae6ff7486b4b81f1ff1fa125e329b
Chris Mears <chris.mears@gmail.com>
2019-01-14 12:20:46 -0800

    Credit World of Tea for chart

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -15,6 +15,21 @@ function Intro(props) {
       </Typography>
       <div style={{ maxWidth: 730, margin: '0 auto' }}>
         <img src={teaProcessingChart} alt="Tea Processing Chart" style={{ width: '100%' }} />
+        <Typography variant="caption" gutterBottom style={{ marginBottom: 10 }}>
+          Tea Processing Chart by{' '}
+          <a href="https://www.worldoftea.org" rel="cc:attributionURL">
+            Tony Gebely
+          </a>{' '}
+          is licensed under a{' '}
+          <a href="http://creativecommons.org/licenses/by-nd/3.0/" rel="license">
+            Creative Commons Attribution-NoDerivs 3.0 Unported License
+          </a><br />
+          . Based on a work at{' '}
+          <a href="https://worldoftea.org/tea-processing-chart/" rel="dct:source">
+            worldoftea.org
+          </a>
+          .
+        </Typography>
       </div>
       <div>
         <Button variant="contained" color="primary" onClick={props.handleClick}>
