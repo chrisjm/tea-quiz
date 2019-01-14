688b80866f21bcf538a96404e87f4851abf7891d
Chris Mears <chris.mears@gmail.com>
2019-01-14 10:39:26 -0800

    Add questions

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,6 +1,6 @@
 const quizQuestions = [
   {
-    question: 'All teas begin as fresh Camellia sinensis leaves.',
+    question: 'All tea begins as fresh Camellia sinensis leaves.',
     answerOptions: [
       {
         content: 'True',
@@ -30,6 +30,96 @@ const quizQuestions = [
       },
     ],
     correctAnswer: 'withering-fixing-drying',
+  },
+  {
+    question: 'What processing steps does yellow tea go through?',
+    answerOptions: [
+      {
+        content: 'Withering, rolling, oxidizing, and drying',
+        value: 'withering-rolling-oxidizing-drying',
+      },
+      {
+        content: 'Withering, fixing, wrapping, drying',
+        value: 'withering-fixing-wrapping-drying',
+      },
+      {
+        content: 'Withering and drying',
+        value: 'withering-drying',
+      },
+    ],
+    correctAnswer: 'withering-fixing-wrapping-drying',
+  },
+  {
+    question: 'What processing steps does black tea go through?',
+    answerOptions: [
+      {
+        content: 'Withering, drying, blackening',
+        value: 'withering-drying-blackening',
+      },
+      {
+        content: 'Withering, fixing, rolling, and fermenting',
+        value: 'withering-fixing-rolling-fermenting',
+      },
+      {
+        content: 'Withering, rolling, oxidizing, and drying',
+        value: 'withering-rolling-oxidizing-drying',
+      },
+    ],
+    correctAnswer: 'withering-rolling-oxidizing-drying',
+  },
+  {
+    question: 'What processing steps does oolong/wulong tea go through?',
+    answerOptions: [
+      {
+        content: 'Withering, fixing, rolling, and fermenting',
+        value: 'withering-fixing-rolling-fermenting',
+      },
+      {
+        content: 'Withering, rolling/bruising, oxidizing, fixing, drying',
+        value: 'withering-rolling-oxidizing-fixing-drying',
+      },
+      {
+        content: 'Withering, rolling, oxidizing, and drying',
+        value: 'withering-rolling-oxidizing-drying',
+      },
+    ],
+    correctAnswer: 'withering-rolling-oxidizing-fixing-drying',
+  },
+  {
+    question: 'What processing steps does white tea go through?',
+    answerOptions: [
+      {
+        content: 'Withering and drying',
+        value: 'withering-drying',
+      },
+      {
+        content: 'Withering, rolling, oxidizing, and drying',
+        value: 'withering-rolling-oxidizing-drying',
+      },
+      {
+        content: 'Withering, rolling/bruising, oxidizing, fixing, drying',
+        value: 'withering-rolling-oxidizing-fixing-drying',
+      },
+    ],
+    correctAnswer: 'withering-drying',
+  },
+  {
+    question: 'What processing steps does fermented or puer tea go through?',
+    answerOptions: [
+      {
+        content: 'Withering, fixing, rolling, and fermenting',
+        value: 'withering-fixing-rolling-fermenting',
+      },
+      {
+        content: 'Withering, drying, fermenting',
+        value: 'withering-drying-fermenting',
+      },
+      {
+        content: 'Withering, rolling, oxidizing, and drying',
+        value: 'withering-rolling-oxidizing-drying',
+      },
+    ],
+    correctAnswer: 'withering-fixing-rolling-fermenting',
   },
 ];
 
